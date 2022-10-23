f1fce593ccccb87919debcd275bab2a59fa293c6
Chris <Whisperswild@gmail.com>
2022-10-23 17:23:16 -0500

    Complete the pixel sketcher project

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 
 
-function buildGrid(count){
-    const container = document.querySelector('#container');
+function buildGrid(){
+    const canvas = document.querySelector('#canvas');
     const gridDiv = document.createElement('div');
+    const gridSize = document.querySelector('#gridSize');
     const docFragment = document.createDocumentFragment(); //stages the elements before writing them to the dom
 
-     for (i=1; i<=count; i++){
+    let count = document.getElementById('gridSize').value;
+    console.log(count);
+    canvas.replaceChildren();
+
+
+
+     for (i=1; i<=count*count; i++){
         const gridDiv = document.createElement('div');
         gridDiv.classList.add('grid');
+
+
+        gridDiv.addEventListener('mousemove', function(e){
+                gridDiv.classList.add('colorPixel');
+        });
+
+
         docFragment.appendChild(gridDiv);
      }
-
-     container.appendChild(docFragment);
+     //canvas.setAttribute('style', 'width: ' + count * 10 + "px");
+     gridDiv.setAttribute('style', 'height: ' + count/10 +'em; width: ' + count/10 + 'em;');
+     canvas.appendChild(docFragment);
 
 }
 
